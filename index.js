a2b6dcd6373be04805f9b5b417c723c707465e5b
abdelhadi <abaditrki@gmail.com>
2024-09-21 20:21:55 -0700

    refactoring

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/template.html");
+  res.sendFile(__dirname + "/template.html");
 });
 
 app.listen(3000, function () {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
